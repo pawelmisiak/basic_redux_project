f2c77c811962c003460f9e92f0d482c3e7a6b252
pawel misiak <pawelmisiak@live.com>
2018-04-17 12:02:09 -0400

    finish work on book container, moving on to action creator

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'C++ For The Unbroken' },
+    { title: 'Eloquent Ruby' },
+    { title: `Who's Code Is It Anyway?` },
+  ];
+}
